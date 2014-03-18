51bf4c9d57b40990d00d88d8febe0f9108320542
Mark Broadhead <orcslayermack@gmail.com>
2014-03-18 15:43:39 -0600

    Making progress. Tried reusing the all the steps. I'm not sure it is going to work...

--- mars/recursion2.asm
+++ mars/recursion2.asm
@@ -1,24 +1,50 @@
 .data
 prompt:		.asciiz "Please enter a number to evaluate: \n"
-factorial:	.asciiz "The factorial of your number is: "
+str:		.asciiz "The factorial of your number is: "
 .text
 main:
 # print prompt
 li $v0, 4
 la $a0, prompt
 syscall
-
 li $v0, 5
 syscall
-move $t0, $v0 # store int in $t0
+move $t0, $v0 # store parameter in $t0
 
-#1-3
+factorial:
+# 1-3
+sw $t0, 0($sp) 		# store parameter
+move $a0, $t0 		# pass parameter
+jal if	 		# jal
+j endFactorial
 
 
 #4-5
+if:
+subu $sp, $sp, 12	# Push the stack 
+sw $ra, 0($sp)		# store return address to stack pointer
+
+# case 1
+ble $a0, 0, endFactorial
+
+# case 2
+# 1-3 steps
+sub $t0, $t0, 1
+jal factorial
+
+# magic
+mult $a0, $v0
+mflo $t0
 
 
+j endFactorial
+# $t0 will be the return variable n
 #6-9
+endFactorial:
+move $v0, $t0
+lw $ra, 0($sp)
+addu $sp, $sp, 12 	# Pop the stack
+jr $ra
 
 
 j end
@@ -27,7 +53,7 @@ j end
 
 end:
 li $v0, 4
-la $a0, factorial
+la $a0, str
 syscall
 li $v0, 1
 move $a0, $t0
